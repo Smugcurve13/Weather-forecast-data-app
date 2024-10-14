a115774d9a8619db55195bf3f584c7a20c3da92a
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-14 14:59:21 +0530

    added temp or sky condition in backend

--- backend.py
+++ backend.py
@@ -3,14 +3,21 @@ import requests
 APIkey = 'bb8c487ce98c5273bfb29176ee18a6a9'
 
 
-def get_data(place,forecasted_days=None,type=None):
+def get_data(place,forecasted_days,type):
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={APIkey}'
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data['list']
+    nr_values = 8*forecasted_days
+    filtered_data = filtered_data[:nr_values]
+    if type == "Temperature":
+        filtered_data = [diction['main']['temp'] for diction in filtered_data]
+    if type == 'Sky':
+        filtered_data = [diction['weather'][0]['main'] for diction in filtered_data]
+    return filtered_data
 
 
 if __name__ == '__main__':
-    print(get_data(place='Noida'))
+    print(get_data('Noida',3,'Sky'))
 
     
